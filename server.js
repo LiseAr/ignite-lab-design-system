f9acbfd160e0a06a735616183143379060aeede4
LiseAr <lisearantes@gmail.com>
2022-10-25 21:38:26 -0300

    srv: configure server

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log('run server')
+// framework minimalista para nodejs
+
+const express = require('express')
+const {} = require('path')
+
+const app = express()
+
+
+app.use('/', 
+  express.static(
+    resolve(
+      __dirname,
+      './dist'
+    )
+  )
+)
+
+
+
+app.listen(process.env || 3000, (err) => {
+  
+  if (err) {return console.log(err)}
+  
+  console.log('Runing....')
+})
