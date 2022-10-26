1844a7b7a6cba5718c934ead98edea04ba520fd0
LiseAr <lisearantes@gmail.com>
2022-10-25 22:11:13 -0300

    server: config __dirname

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 // framework minimalista para nodejs
 import express from 'express'
 import { resolve } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 
-app.use('/', 
-  express.static(
-    resolve(
-      __dirname,
-      './dist'
-    )
-  )
-)
+const __filename = fileURLToPath(import.meta.url)
 
+const __dirname = path.dirname(__filename)
 
+app.use('/', express.static(resolve(__dirname, './dist')))
+
+app.get('posts', (req, res) => {
+  res.send({
+    title: 'título',
+    conteudo: 'conteudo',
+  })
+})
 
 app.listen(process.env || 3000, (err) => {
-  
-  if (err) {return console.log(err)}
-  
+  if (err) {
+    return console.log(err)
+  }
+
   console.log('Runing....')
 })
